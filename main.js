55548dd06388f9fd7621c81bedfbc6abd4a1a088
TheMoneyMe306 <alancomputer2027@gmail.com>
2026-09-19 11:47:23 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -19,12 +19,12 @@ const chickenMenus = [
   { id: "kai-krathiam", num: 3, name: "ข้าวไก่กระเทียม", price: 45, img: "kai-krathiam.jpg" },
   { id: "kai-prikklua", num: 4, name: "ข้าวไก่คั่วพริกเกลือ", price: 45, img: "kai-khua-prik-klua.jpg" },
   { id: "kai-samunphrai", num: 5, name: "ข้าวไก่นึ่งสมุนไพร + น้ำจิ้มซีฟู้ด", price: 45, img: "kai-neung-samunphrai.jpg" },
-  { id: "kai-samunphrai-kap", num: 6, name: "ไก่นึ่งสมุนไพร เป็นกับข้าว (ปีกไก่ 2 + น่องไก่ 1 ชิ้น)", price: 50, img: "kai-peek-nong-samunphrai.jpg", side: true },
-  { id: "kai-saphok-samunphrai", num: 11, name: "สะโพกไก่นึ่งสมุนไพร (ชิ้นโตๆ)", price: 40, img: "kai-saphok-samunphrai.jpg", side: true, tag: "ชิ้นละ 40.-" },
+  { id: "kai-samunphrai-kap", num: 6, name: "ไก่นึ่งสมุนไพร เป็นกับข้าว (ปีกไก่ 2 + น่องไก่ 1 ชิ้น)", price: 50, img: "kai-peek-nong-samunphrai.png", side: true },
+  { id: "kai-saphok-samunphrai", num: 11, name: "สะโพกไก่นึ่งสมุนไพร (ชิ้นโตๆ)", price: 40, img: "kai-saphok-samunphrai.png", side: true, tag: "ชิ้นละ 40.-" },
 ];
 
 const shrimpMenus = [
-  { id: "kung-kari", num: 7, name: "ข้าวกุ้งผัดผงกะหรี่", price: 45, img: "kung-phad-phong-kari.jpg" },
+  { id: "kung-kari", num: 7, name: "ข้าวกุ้งผัดผงกะหรี่", price: 50, img: "kung-phad-phong-kari.jpg" },
   { id: "kung-muk-kari", num: 8, name: "ข้าวกุ้ง+หมึกผัดผงกะหรี่", price: 50, img: "kung-phad-phong-kari.jpg", note: "ใช้รูปชั่วคราว" },
   { id: "kung-prikklua", num: 9, name: "ข้าวกุ้งคั่วพริกเกลือ", price: 50, img: "kung-khua-prik-klua.jpg" },
   { id: "kung-krathiam", num: 10, name: "ข้าวกุ้งกระเทียม", price: 50, img: "kung-krathiam.jpg" },
